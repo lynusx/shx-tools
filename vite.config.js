1e57e7b53f7637efaeab2f1d08d6a44645fe8018
wpp <lynusx@hotmail.com>
2025-06-05 10:11:49 +0800

    feat: 集成PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon.ico',
+        'favicon.svg',
+        'apple-touch-icon-180x180.png',
+      ],
+      manifest: {
+        name: 'SHX Tools',
+        short_name: 'SHX',
+        description: '🐂🐎大礼包、RCA EL图片拷贝工具',
+        theme_color: '#ffffff',
+        background_color: '#f0f0f0',
+        start_url: '/',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+        ],
+      },
+    }),
+  ],
 })
